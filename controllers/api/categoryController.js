2dd4cd0994c94537fd4dc90ea5ceb2d991994350
Rhonda Stokes <rhonda@aerostokes.com>
2023-05-09 15:44:55 -0700

    update routes with Template data when not logged in

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -1,78 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { Category, TemplateCategory } = require('../../models');
 
-const { User, Category } = require('../../models');
+// Routes for /api/categories
 
+// Get all Categories
 router.get("/", async (req, res) => {
-    if (!req.session.loggedIn) return res.status(403).json({ msg: 'You are not logged in!' });
-    const userId = req.session.UserId;
-
-    // const currentUser = await User.findByPk(UserId);
-    // const categories = await currentUser.getCategories();
-
-    // const currentUser = await User.findByPk(userId, {include: Category});
-    // const categories = currentUser.Categories;
-
-    const categories = await Category.findAll({ where: { UserId: userId }});
-
-    if (categories.length === 0) {
-        return res.status(404).json({ msg: "No Categories found" });
-    }
-    res.json(categories);
-});
-
-// Get Category by id
-router.get("/:id", (req, res) => {
-    if (!req.session.loggedIn) {
-        return res.status(403).json({ msg: "Login required" })
-    } else {
-        Category.findByPk(req.params.id).then(categoryObj => {
-            if (!categoryObj) {
-                return res.status(404).json({ msg: "CategoryId not found" });
-            } else if (categoryObj.UserId !== req.session.UserId) {
-                return res.status(403).json({ msg: "Not authorized for this UserId" })
-            } else {
-                return res.json(categoryObj);
-            };
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json({ msg: "Error Occurred", err });
-        });
-    };
-});
-
-router.post("/", async (req, res) => {
-    if (!req.session.loggedIn) return res.status(403).json({ msg: 'You are not logged in!' });
-    const currentUser = await User.findByPk(req.session.UserId);
-
     try {
-        const category = await currentUser.createCategory({
-            name: req.body.name,
-            emoji: req.body.emoji,
-            color: req.body.color,
-        });
-
-        res.json({ msg: "Successfully created", category });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ msg: "Error Occurred", error });
-    }
-});
-
-// Put update Category by id
-router.put("/:id", async (req, res) => {
-    if (!req.session.loggedIn) {
-        return res.status(403).json({ msg: "Login required" }); 
-    };
-    try {
-        const categoryEntry = await Category.findByPk(req.params.id);
-        if (!categoryEntry) {
-            return res.status(404).json({ msg: "CategoryId not found" });
-        } else if (req.session.UserId !== categoryEntry.UserId) {
-            return res.status(403).json({ msg: "Not authorized for this UserId" });
+        if (!req.session.loggedIn) {
+            const templateCategoriesArr = await TemplateCategory.findAll();
+            return res.json(templateCategoriesArr);
         } else {
-            await categoryEntry.update(req.body);
-            return res.json({ msg: "Successfully updated" });
+            const categoriesArr = await Category.findAll({ 
+                where: { UserId: req.session.UserId },
+            });
+            return res.json(categoriesArr);
         };
     } catch (err) {
         console.log(err);
@@ -80,22 +22,93 @@ router.put("/:id", async (req, res) => {
     };
 });
 
-router.delete("/:id", async (req, res) => {
-    if (!req.session.loggedIn) return res.status(403).json({ msg: 'You are not logged in!' });
-    
+// Get Category by id
+router.get("/:id", async (req, res) => {
     try {
-        // Require check for UserId match before deleting
-        const categoryEntry = await Category.findByPk(req.params.id);
-        if (!categoryEntry) {
-            return res.status(404).json({ msg: "CategoryId not found" });
-        } else if (req.session.UserId !== categoryEntry.UserId) {
-            return res.status(403).json({ msg: "Not authorized for this UserId" });
+        if (!req.session.loggedIn) {
+            const templateCategoryObj = await TemplateCategory.findByPk(req.params.id);
+            if (!templateCategoryObj) {
+                return res.status(404).json({ msg: "TemplateCategoryId not found" });
+            } else {
+                return res.json(templateCategoryObj);
+            };
         } else {
-            await categoryEntry.destroy();
-            res.json({ msg: `Successfully deleted Category with ID: ${req.params.id}`});
+            const categoryObj = await Category.findByPk(req.params.id);
+            if (!categoryObj) {
+                return res.status(404).json({ msg: "CategoryId not found" });
+            } else if (categoryObj.UserId !== req.session.UserId) {
+                return res.status(403).json({ msg: "Not authorized for this UserId" });
+            } else {
+                return res.json(categoryObj);
+            };
         };
-    } catch (error) {
-        res.status(500).json({ msg: "Error Occurred", error });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "Error Occurred", err });
+    };
+});
+
+// Post new Category
+router.post("/", async (req, res) => {
+    try {
+        if (!req.session.loggedIn) {
+            return res.status(403).json({ msg: "Login required" });
+        } else {
+            const category = await Category.create({
+                name: req.body.name,
+                emoji: req.body.emoji,
+                color: req.body.color,
+                UserId: req.session.UserId,
+            });
+            res.json({ msg: "Successfully created", category });
+        };
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ msg: "Error Occurred", err });
+    }
+});
+
+// Update Category by id
+router.put("/:id", async (req, res) => {
+    try {
+        if (!req.session.loggedIn) {
+            return res.status(403).json({ msg: "Login required" }); 
+        } else {
+            const categoryEntry = await Category.findByPk(req.params.id);
+            if (!categoryEntry) {
+                return res.status(404).json({ msg: "CategoryId not found" });
+            } else if (req.session.UserId !== categoryEntry.UserId) {
+                return res.status(403).json({ msg: "Not authorized for this UserId" });
+            } else {
+                await categoryEntry.update(req.body);
+                return res.json({ msg: `Successfully updated Category with id: ${req.params.id}` });
+            };
+        };
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "Error Occurred", err });
+    };
+});
+
+// Delete Category by id
+router.delete("/:id", async (req, res) => {
+    try {
+        if (!req.session.loggedIn) {
+            return res.status(403).json({ msg: "Login required" }); 
+        } else {
+            const categoryEntry = await Category.findByPk(req.params.id);
+            if (!categoryEntry) {
+                return res.status(404).json({ msg: "CategoryId not found" });
+            } else if (req.session.UserId !== categoryEntry.UserId) {
+                return res.status(403).json({ msg: "Not authorized for this UserId" });
+            } else {
+                await categoryEntry.destroy();
+                res.json({ msg: `Successfully deleted Category with id: ${req.params.id}`});
+            };
+        };
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "Error Occurred", err });
     }
 });
 
