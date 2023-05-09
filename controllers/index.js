ca52c0faa96a1b5b7084fad1a7d973566f10bdc2
Brent Houston <brentjustinhouston@gmail.com>
2023-05-09 10:58:45 -0700

    added dashboar.hbs

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const apiRoutes = require("./api");
 
 router.get("/", (req, res) => {
-    res.render('../views/home.handlebars');
+    res.render('home');
 });
 
 router.use("/api", apiRoutes);
---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// fetch request to get all stickies for the loged in user
+// .then(data) => {
+// append to page 
+// }
