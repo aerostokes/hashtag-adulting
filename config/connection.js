2dc860b94b696c45f59b4a1eeb720373974a9524
Taylor Watters <taylorbbigelow@gmail.com>
2023-05-09 12:52:39 -0700

    adds sendgrid

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
 const Sequelize = require("sequelize");
-require("dotenv").config();
  
 const sequelize = new Sequelize(
     process.env.DB_NAME,
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// !! DO BEFORE ALL ELSE
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
