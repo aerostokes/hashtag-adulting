fb6c37c9d19c5670a7b96b3a21e8160114d04cc8
Alex Moriyama <alex.moriyama@gmail.com>
2023-05-10 12:20:37 -0700

    updated sign up jscript

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,19 +2,18 @@ const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("Sign Up",e=>{
     e.preventDefault();
     const userObj = {
-        email:document.querySelector("#signup-email").value,
-        password:document.querySelector("#signup-password").value,
+        email:document.querySelector("#signup-email").value.trim,
+        password:document.querySelector("#signup-password").value.trim,
+        passwordconfirm:document.querySelector("#signup-password-confirmation").value.trim
     }
-    fetch("/api/users/login",{
+    fetch("/signup",{
         method:"POST",
         body:JSON.stringify(userObj),
         headers:{
             "Content-Type":"application/json"
         }
     }).then(res=>{
-        if(res.ok){
-           location.href = "/"
-        } else {
+        if(!res.ok){
             alert("Invalid sign up")
         }
     })
