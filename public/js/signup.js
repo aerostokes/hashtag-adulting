ee156fced4ae3dbfc79ee0f7ca63bfbbcaa69e53
Alex Moriyama <alex.moriyama@gmail.com>
2023-05-10 12:32:28 -0700

    fix jscript signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,12 +1,17 @@
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("Sign Up",e=>{
     e.preventDefault();
+    const password = document.querySelector("#signup-password").value.trim
+    const passwordconfirm = document.querySelector("#signup-password-confirmation").value.trim
+    if (password === passwordconfirm){
+        alert("Your password doesn't match!")
+    } else {
     const userObj = {
         email:document.querySelector("#signup-email").value.trim,
         password:document.querySelector("#signup-password").value.trim,
         passwordconfirm:document.querySelector("#signup-password-confirmation").value.trim
     }
-    fetch("/signup",{
+    fetch("/api/users",{
         method:"POST",
         body:JSON.stringify(userObj),
         headers:{
@@ -17,4 +22,5 @@ signupForm.addEventListener("Sign Up",e=>{
             alert("Invalid sign up")
         }
     })
+}
 })
